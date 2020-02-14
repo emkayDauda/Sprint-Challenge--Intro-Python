4a3707a51abe3e672ce8e5cd912a72e5e603d43a
Maaruf Dauda <mkdauda.work@gmail.com>
2020-02-14 09:59:24 +0100

    :sparkles: Complete comp.py... All tests pass

--- src/comp/comp.py
+++ src/comp/comp.py
@@ -79,5 +79,5 @@ print(g)
 # Write a list comprehension that contains the square root of all the ages.
 print("Square root of ages:")
 import math
-h = []
+h = list(map(lambda human: math.sqrt(human.age), humans))
 print(h)
