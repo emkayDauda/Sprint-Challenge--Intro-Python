e7412fc60453e54cbc67a5a94e669191e8b64b95
Maaruf Dauda <mkdauda.work@gmail.com>
2020-02-14 09:56:48 +0100

    :construction: Progress on comp.py. Pass 7/8 tests

--- src/comp/comp.py
+++ src/comp/comp.py
@@ -73,7 +73,7 @@ print(f)
 # list, except with all the names uppercase and the ages with 5 added to them.
 # The "humans" list should be unmodified.
 print("All names uppercase:")
-g = []
+g = list(map(lambda human: Human(human.name.upper(), human.age + 5), humans))
 print(g)
 
 # Write a list comprehension that contains the square root of all the ages.
